fb69b85b81cbcb0f3c472ef66aba8f0bf9d19235
Charan5253 <89011688+Charan5253@users.noreply.github.com>
2023-04-24 09:25:02 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,13 +5,13 @@
 
 import os
 
-bot_token = os.environ["BOT_TOKEN"]
+bot_token = os.environ["5911160230:AAFk86M5NjtvPxqFUd9mXv_W4YlGHIGO8k8"]
 
-api_id = os.environ["API_ID"]
+api_id = os.environ["11480401"]
 
-api_hash = os.environ["API_HASH"]
+api_hash = os.environ["e151e5dc145d2b42accde41a37207201"]
 
-pdisk_api = os.environ["PDISK_API"]
+pdisk_api = os.environ["https://pdisk.pro/api/account/info?key=1714c294o4yd68xtvw22"]
 
 try:
     connection_string = os.environ["MONGO_CON_STRING"]
